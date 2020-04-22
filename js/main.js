791f35ba3a6bd001a77de885e77dd24204ddf769
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2020-04-22 23:02:51 +0400

    Do ToDo 2020-04-22

--- js/main.js
+++ js/main.js
@@ -1,25 +1,63 @@
 const formTask = document.querySelector("#newTaskForm");
 const taskList = document.querySelector("#taskList");
 const cardContainer = document.querySelector("#cardContainer");
+const input =  document.querySelector("#addNewTask");
 
 formTask.addEventListener("submit", function(event){
     event.preventDefault();
 
-    const taskText = document.querySelector("#addNewTask").value;
+    const taskText = input.value.trim();
+    const taskHTML = `<li class="list-group-item d-flex justify-content-between"><span contenteditable="true" class="task-title">${taskText}</span>
+    <div id="buttons-task">
+        <button type="button" data-action="ready" class="btn btn-light align-self-end btn-done"></button>
+        <button type="button" data-action="delete-task" class="btn btn-light align-self-end btn-delete"></button>
+    </div></li>`
+    
+    taskList.insertAdjacentHTML("afterbegin", taskHTML);
+    input.value = "";
+    input.focus;
+    
+    hideEmptyList();
 
-    const taskHTML = `<li class="list-group-item d-flex justify-content-between"><span class="task-title">${taskText}</span><button type="button" data-action="delete-task" class="btn btn-light align-self-end">Удалить</button></li>`
-    document.querySelector("#addNewTask").value = "";
-
-    taskList.insertAdjacentHTML("beforeend", taskHTML);
     allerts('add', taskText);
 });
 
 taskList.addEventListener("click", function(event){
+     // находим родительский тег <li>
+     const parentElement = event.target.closest(".list-group-item");
+     const taskName = parentElement.firstChild.innerHTML;
+
     if (event.target.getAttribute("data-action") == "delete-task"){
-        const liElement = event.target.parentElement;
-        const taskName = liElement.firstChild.innerHTML;
-        liElement.remove();
+        parentElement.remove();
+        hideEmptyList();
         allerts('remove', taskName);
+    }
+    else if (event.target.getAttribute("data-action") == "ready"){
+        // Добавляем/убираем  к тегу span дополнительный класс
+        parentElement.querySelector(".task-title").classList.toggle("task-title-done");
+        // Добавляем/убираем кнопке дополнительный класс
+        event.target.classList.toggle("ready");
+
+        if (event.target.classList.contains("ready")){
+            // Переносим выполненную задачу в конец списка
+            taskList.insertAdjacentElement("beforeend", parentElement);
+
+            // Убираем у тега span атрибут contenteditable 
+            parentElement.querySelector(".task-title").setAttribute("contenteditable", "false");
+
+            // Показываем нотификацию
+            allerts('done', taskName);
+        }
+        else {
+             // Переносим выполненную задачу в начало списка
+             taskList.insertAdjacentElement("afterbegin", parentElement);
+
+             // Добавляем у тега span атрибут contenteditable 
+             parentElement.querySelector(".task-title").setAttribute("contenteditable", "true");
+
+             // Показываем нотификацию
+            allerts('add', taskName);
+        }
     }
 });
 
@@ -37,7 +75,7 @@ function allerts(val, taskName){
             allertsHTML = `<div id="removeAllert" class="alert alert-danger" role="alert">Задача ${taskName} удалена!</div>`;
             break;
         case 'done': 
-            allertsHTML = `<div class="alert alert-success" role="alert">Задача ${taskName} выполнена!</div>`;
+            allertsHTML = `<div id="removeAllert" class="alert alert-success" role="alert">Задача ${taskName} выполнена!</div>`;
             break;
     }
 
@@ -46,4 +84,13 @@ function allerts(val, taskName){
     setTimeout(() => {
         removeAllert.remove();
     }, 800);
+};
+
+function hideEmptyList(){
+    if (taskList.childElementCount > 1) {
+        document.querySelector("#emptyListItem").style.display = "none";
+    }
+    else {
+        document.querySelector("#emptyListItem").style.display = "block";
+    }
 };
